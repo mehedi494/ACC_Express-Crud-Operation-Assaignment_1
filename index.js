0ac6878021a6397ae47d851838006f76974fcec0
mehedi494 <ehasanmunna21@gmail.com>
2022-10-01 14:55:28 +0600

    add env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require('cors');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const router = require ("./Routes/v1/user.routes")
 
 app.use(cors());
